d760015b4a5ce31d6da5a30890b599a8e1312be5
hitode909 <hitode909@gmail.com>
2013-06-12 14:39:33 +0900

    documented about configuring

--- README.md
+++ README.md
@@ -11,3 +11,13 @@
 (highlight-unique-symbol t)
 ```
 
+## Configuring
+
+You can configure these settings with `M-x customize-group RET highlight-unique-symbol RET`.
+
+<dl>
+  <dt>highlight-unique-symbol:interval</dt>
+  <dd>Interval to check symbol at cursor.</dd>
+  <dt>highlight-unique-symbol:face</dt>
+  <dd>Face of unique symbols.</dd>
+</dl>
--- highlight-unique-symbol.el
+++ highlight-unique-symbol.el
@@ -26,6 +26,14 @@
 ;;
 ;; (require 'highlight-unique-symbol)  ;; Not necessary if using ELPA package
 ;; (highlight-unique-symbol t)
+;;
+;; You can configure these settings with `M-x customize-group RET highlight-unique-symbol RET`.
+;;
+;; `highlight-unique-symbol:interval`
+;; Interval to check symbol at cursor.
+;;
+;; `highlight-unique-symbol:face`
+;; Face of unique symbols.
 
 ;;; Code:
 
